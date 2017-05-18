d690da795e081ca3f652726d025028a8ded01bda
Mauro André Barros Mazzola <mauroandre.mazzola@hotmail.com>
2017-05-18 19:00:16 -0300

    Initial commit

---
+++ app/src/main/java/mauroandremazzola/com/br/bragancatechday2017/views/home/home_tabs/about/AboutFragment.java
@@ -0,0 +1,10 @@
+package mauroandremazzola.com.br.bragancatechday2017.views.home.home_tabs.about;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by mauroandremazzola on 18/05/17.
+ */
+
+public class AboutFragment extends Fragment {
+}
---
+++ app/src/main/java/mauroandremazzola/com/br/bragancatechday2017/views/home/home_tabs/video/VideosFragment.java
@@ -0,0 +1,10 @@
+package mauroandremazzola.com.br.bragancatechday2017.views.home.home_tabs.video;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by mauroandremazzola on 18/05/17.
+ */
+
+public class VideosFragment extends Fragment {
+}
